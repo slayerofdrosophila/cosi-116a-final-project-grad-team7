5fa81ba3cb71098d7cd33bc40f82e9e371de165f
slayerofdrosophila <joshliu@brandeis.edu>
2023-12-01 00:53:30 -0500

    hover effects on map

--- js/hoverableMap.js
+++ js/hoverableMap.js
@@ -6,14 +6,23 @@ d3.xml("../images/boston.svg").then((data) => {
 });
 
 function hoverEffects() {
+  const tooltip = d3.select("#tooltip");
+
   d3.selectAll("#map-container svg g#areas path")
-    .on("click", function (event, d) {
-      // Change fill color on click
-      d3.selectAll("#map-container svg g#areas path").style("fill", ""); // Reset other paths' color
-      d3.select(this).style("fill", "lightblue"); // Highlight clicked path
-    })
     .on("mouseover", function (event, d) {
-      const pathId = d3.select(this).attr("id");
-      console.log("Area Name: " + pathId); // Display the area name (you can replace this with code to show a tooltip)
+      d3.select(this).style("fill", "lightblue");
+      tooltip.transition()
+             .duration(200)
+             .style("opacity", .9);
+      tooltip.html("Area Name: " + d3.select(this).attr("id"))
+             .style("left", (event.pageX + 10) + "px")
+             .style("top", (event.pageY - 28) + "px");
+    })
+    .on("mouseout", function (d) {
+      d3.select(this).style("fill", "");
+      tooltip.transition()
+             .duration(500)
+             .style("opacity", 0);
     });
 }
+
