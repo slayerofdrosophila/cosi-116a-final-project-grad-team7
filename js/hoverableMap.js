84ceea23a06a83a592fa675bf3fa5696ad795bf6
Trista Lu <t09b02307@ntu.edu.tw>
2023-11-30 21:20:44 -0500

    add hoverable effect on map

--- js/hoverableMap.js
+++ js/hoverableMap.js
@@ -2,4 +2,18 @@
 // Load the SVG file
 d3.xml("../images/boston.svg").then((data) => {
   d3.select("#map-container").node().append(data.documentElement);
+  hoverEffects();
 });
+
+function hoverEffects() {
+  d3.selectAll("#map-container svg g#areas path")
+    .on("click", function (event, d) {
+      // Change fill color on click
+      d3.selectAll("#map-container svg g#areas path").style("fill", ""); // Reset other paths' color
+      d3.select(this).style("fill", "lightblue"); // Highlight clicked path
+    })
+    .on("mouseover", function (event, d) {
+      const pathId = d3.select(this).attr("id");
+      console.log("Area Name: " + pathId); // Display the area name (you can replace this with code to show a tooltip)
+    });
+}
